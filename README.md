7a07dc63f9b1f491fe1ebdb4f3e8f0cba51c6e08
Ilya Paripsa <iparips@gmail.com>
2024-03-12 14:05:09 +1100

    Add function to fetch issues from a github repo

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 `source bin/activate` to activate the virtual environment
 `deactivate` to deactivate it (calls function exported by the activate script)
+`pipenv install <package-name>`
---
+++ src/datasets.py
@@ -0,0 +1,52 @@
+import time
+import math
+import pandas as pd
+import os
+import requests
+
+from pathlib import Path
+from tqdm import tqdm 
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+github_token = os.environ.get("GITHUB_TOKEN", "default")
+
+headers = {"Authorization": f"token {github_token}"}
+
+def fetch_issues(
+    owner="huggingface",
+    repo="datasets",
+    num_issues=10_000,
+    rate_limit=5_000,
+    issues_path=Path("."),
+):
+    if not issues_path.is_dir():
+        issues_path.mkdir(exist_ok=True)
+
+    batch = []
+    all_issues = []
+    per_page = 100  # Number of issues to return per page
+    num_pages = math.ceil(num_issues / per_page)
+    base_url = "https://api.github.com/repos"
+
+    for page in tqdm(range(num_pages)):
+        # Query with state=all to get both open and closed issues
+        query = f"issues?page={page}&per_page={per_page}&state=all"
+        issues = requests.get(f"{base_url}/{owner}/{repo}/{query}", headers=headers)
+        batch.extend(issues.json())
+
+        if len(batch) > rate_limit and len(all_issues) < num_issues:
+            all_issues.extend(batch)
+            batch = []  # Flush batch for next time period
+            print(f"Reached GitHub rate limit. Sleeping for one hour ...")
+            time.sleep(60 * 60 + 1)
+
+    all_issues.extend(batch)
+    df = pd.DataFrame.from_records(all_issues)
+    df.to_json(f"{issues_path}/{repo}-issues.jsonl", orient="records", lines=True)
+    print(
+        f"Downloaded all the issues for {repo}! Dataset stored at {issues_path}/{repo}-issues.jsonl"
+    )
+
+fetch_issues()
