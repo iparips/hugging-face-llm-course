bad1959afe703c15cf5d5673f5cf23cde62d1e5f
Ilya Paripsa <iparips@gmail.com>
2024-03-12 14:14:50 +1100

    Downloaded the date sets

--- src/datasets.py
+++ src/datasets.py
@@ -17,32 +17,24 @@ headers = {"Authorization": f"token {github_token}"}
 def fetch_issues(
     owner="huggingface",
     repo="datasets",
-    num_issues=10_000,
-    rate_limit=5_000,
+    num_issues=5_000,
     issues_path=Path("."),
 ):
     if not issues_path.is_dir():
         issues_path.mkdir(exist_ok=True)
 
-    batch = []
     all_issues = []
     per_page = 100  # Number of issues to return per page
     num_pages = math.ceil(num_issues / per_page)
     base_url = "https://api.github.com/repos"
 
+
     for page in tqdm(range(num_pages)):
         # Query with state=all to get both open and closed issues
         query = f"issues?page={page}&per_page={per_page}&state=all"
         issues = requests.get(f"{base_url}/{owner}/{repo}/{query}", headers=headers)
-        batch.extend(issues.json())
+        all_issues.extend(issues.json())
 
-        if len(batch) > rate_limit and len(all_issues) < num_issues:
-            all_issues.extend(batch)
-            batch = []  # Flush batch for next time period
-            print(f"Reached GitHub rate limit. Sleeping for one hour ...")
-            time.sleep(60 * 60 + 1)
-
-    all_issues.extend(batch)
     df = pd.DataFrame.from_records(all_issues)
     df.to_json(f"{issues_path}/{repo}-issues.jsonl", orient="records", lines=True)
     print(
